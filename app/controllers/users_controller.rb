084c3680dfae9076b2e5ea6b17cd69a640d00f18
Avijit Karmaker <ajkacca@gmail.com>
2020-11-10 12:31:06 +0200

    new user registration add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  before_action :authorization, only: [:show]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid? && @user.save
+      redirect_to new_session_path, notice: 'Sign up successful, login using your credentials'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
 end
