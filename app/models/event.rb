c11b39cb380c051c62cd036ae2da78080e363a43
Avijit Karmaker <ajkacca@gmail.com>
2020-11-10 12:26:44 +0200

    scope and validation add

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+
+  validates :title, :description, :date, presence: true
+  belongs_to :creator, class_name: 'User', foreign_key: :user_id
+  has_many :attendances, dependent: :destroy
+  has_many :attendees, through: :attendances, class_name: 'User', foreign_key: :user_id
+
+  scope :past, -> { where('date<?', Time.now).order('created_at DESc') }
+  scope :upcoming, -> { where('date>?', Time.now).order('created_at DESc') }
+
 end
